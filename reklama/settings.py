38ce12e8a6495a7b8abda912dfa1342155105628
baetov97 <nurgazy.baetov@gmail.com>
2020-12-30 16:42:34 +0600

    staticfiles

--- reklama/settings.py
+++ reklama/settings.py
@@ -136,16 +136,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # 配信用のディレクトリ
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # 「manage.py collectstatic」のコピー先
-STATIC_ROOT = '/app/static'
+# STATIC_ROOT = '/app/static'
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
