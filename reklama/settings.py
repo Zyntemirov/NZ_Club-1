7716d3e20f3dcb32629dbd0cd0eaefa1689ebcbd
baetov97 <nurgazy.baetov@gmail.com>
2021-03-12 14:56:13 +0600

    MEDIA_URL and MEDIA_ROOT is fixed

--- reklama/settings.py
+++ reklama/settings.py
@@ -143,15 +143,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
-
-# STATIC_ROOT = '/app/static'
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
