7dfc6dca40c7075cef0dd1b7a88a565423edcead
baetov97 <nurgazy.baetov@gmail.com>
2021-04-06 02:22:07 +0600

    myvideo admin

--- video/admin.py
+++ video/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+
+# Register your models here.
+from django.db.models import Q
+from django.forms.models import BaseModelFormSet
+
+from accounts.models import User
 from video.models import *
 
 
@@ -71,6 +77,56 @@ class VideoAdmin(admin.ModelAdmin):
             super().save_model(request, obj, form, change)
 
 
+@admin.register(MyVideo)
+class MyVideoAdmin(admin.ModelAdmin):
+    list_display = ['owner', 'title', 'category', 'status', 'is_active']
+    list_display_links = ['owner', 'title', 'category']
+    search_fields = ['title', 'text']
+    list_filter = ['owner__username']
+    exclude = ('favorites', 'views', 'is_active', 'watched_videos')
+    list_per_page = 50
+    autocomplete_fields = ['owner', ]
+    list_select_related = ['owner']
+    # list_editable = ['status', ]
+    inlines = [TariffInline]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(owner=request.user)
+
+    def make_activation(self, request, queryset):
+        queryset.update(status='2')
+
+    make_activation.short_description = 'Активировать'
+
+    def get_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return ['title', 'text', 'phone_1', 'phone_2', 'phone_3', 'instagram', 'facebook', 'web_site',
+                    'video', 'category', 'type', 'is_top', 'image', 'owner', 'status']
+        else:
+            return ['title', 'text', 'phone_1', 'phone_2', 'phone_3', 'instagram', 'facebook', 'web_site',
+                    'video', 'category', 'type', 'image', 'owner', ]
+
+    def get_inline_instances(self, request, obj=None):
+        if request.user.is_superuser:
+            return [inline(self.model, self.admin_site) for inline in self.inlines]
+        return []
+
+    def save_model(self, request, obj, form, change):
+        if request.user.is_superuser:
+            if obj.id == None:
+                obj.status = '2'
+                obj.is_active = True
+                users = User.objects.filter(Q(profile_balance_lte=10) | Q(profile_withdrawn_balance=0))
+                devices = FCMDevice.objects.filter(user__in=users)
+                devices.send_message(title="Новое видео🔥", body="Кликните сюда чтобы посмотреть видео " + self.title)
+                super().save_model(request, obj, form, change)
+            else:
+                super().save_model(request, obj, form, change)
+
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
     search_fields = ['title']
@@ -135,5 +191,3 @@ admin.site.register(Request, RequestAdmin)
 admin.site.register(Banner, BannerAdmin)
 admin.site.register(ViewBanner, ViewBannerAdmin)
 admin.site.register(VideoTraining, VideoTrainingAdmin)
-
-
